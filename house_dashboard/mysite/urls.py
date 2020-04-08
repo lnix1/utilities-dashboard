5d14f04d6583b27388f10da259ba56263d41f787
lnix1 <louis.nix2@gmail.com>
2020-04-07 22:28:27 -0400

    beginning to integrate scraper

--- house_dashboard/mysite/urls.py
+++ house_dashboard/mysite/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('index.html', views.homepage, name='homepage2'),
     path('utilities.html', views.utilities, name='utilities'),
-    path('view_past_orders.html', views.view_past_orders, name='view_past_orders'),
     path('groceries.html', views.groceries, name='groceries')
 ]
